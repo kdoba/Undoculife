646cad10bbd938e15f699c0e36999e19fe4ff171
Robert Hagan <rdhagan@gmail.com>
2014-03-16 15:30:48 -0700

    add initial scripts and prefabs

---
+++ Assets/Scripts/FollowTarget.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Updates the object's position to be an offset from its target.
+/// </summary>
+public class FollowTarget : MonoBehaviour {
+
+	/// <summary>
+	/// The target to follow.
+	/// </summary>
+	public GameObject target;
+
+	/// <summary>
+	/// The offset from the target.
+	/// </summary>
+	public Vector3 offset = new Vector3(0, 0, -3);
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = target.transform.position + offset;
+	}
+}
