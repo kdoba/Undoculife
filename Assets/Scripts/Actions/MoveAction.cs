d2a748a1667c0a174fcf9ca098a517d421a3e22c
Robert Hagan <rhagan@zynga.com>
2014-04-06 12:56:16 -0700

    Add MoveAction

---
+++ Assets/Scripts/Actions/MoveAction.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Action to move a game object
+/// </summary>
+[System.Serializable]
+public class MoveAction : BaseAction {
+
+	/// <summary>
+	/// The position to move to.
+	/// </summary>
+	public Vector3 pos;
+
+	/// <summary>
+	/// Whether the "other" game object passed into the action.
+	/// </summary>
+	public bool moveOther = false;
+
+	/// <summary>
+	/// Execute the action.
+	/// </summary>
+	/// <param name="obj">The object executing the action.</param>
+	override public void Execute(GameObject obj) {
+		if (moveOther) {
+			obj.transform.position = pos;
+		} else {
+			gameObject.transform.position = pos;
+		}
+	}
+}
